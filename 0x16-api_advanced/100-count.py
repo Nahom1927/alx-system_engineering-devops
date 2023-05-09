a48253795ea4426c4c9ac49daef23787b066e978
Nahom1927 <yheys898184@gmail.com>
2023-05-09 23:25:12 +0300

    all

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,46 +1,52 @@
 #!/usr/bin/python3
-"""Module with top_ten function"""
-import re
+"""
+Queries the Reddit API, parses the title of all hot articles,
+and prints a sorted count of given keywords
+"""
 import requests
-import sys
 
 
-def count_words(subreddit, word_list, after="", to_print={}):
-    """Queries the Reddit API and returns a list containing the titles of all
-    hot articles for a given subreddit."""
+def count_words(subreddit, word_list, instances={}, after="", count=0):
+    """prints a sorted count of given keywords (case-insensitive,
+    delimited by spaces."""
 
-    if after == "":
+    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    }
+    params = {
+        "after": after,
+        "count": count,
+        "limit": 100
+    }
+    response = requests.get(url, headers=headers, params=params,
+                            allow_redirects=False)
+    try:
+        results = response.json()
+        if response.status_code == 404:
+            raise Exception
+    except Exception:
+        print("")
+        return
+
+    results = results.get("data")
+    after = results.get("after")
+    count += results.get("dist")
+    for c in results.get("children"):
+        title = c.get("data").get("title").lower().split()
         for word in word_list:
-            to_print[word] = 0
+            if word.lower() in title:
+                times = len([t for t in title if t == word.lower()])
+                if instances.get(word) is None:
+                    instances[word] = times
+                else:
+                    instances[word] += times
 
-    url = "https://www.reddit.com/r/{}/hot.json{}".format(subreddit, after)
-    json_obj = requests.get(url, headers={'User-Agent': 'My User Agent 1.0'})
-
-    if json_obj.status_code != 404:
-
-        dict_obj = json_obj.json()
-        list_obj = dict_obj.get('data').get('children')
-
-        for each in list_obj:
-            title = each.get('data').get('title')
-            tit_list = title.split()
-            for word in word_list:
-                c = re.compile(r"^{}$".format(word), re.I)
-                for each_tit_w in tit_list:
-                    res = c.findall(each_tit_w)
-                    to_print[word] += len(res)
-
-        next_fullname = dict_obj.get('data').get('after')
-        after = "?after={}".format(next_fullname)
-
-        if next_fullname is not None:
-            count_words(subreddit, word_list, after, to_print)
-        else:
-            sorted_l = sorted(to_print.items(), key=lambda x: x[1])
-            sorted_l.reverse()
-
-            for x in sorted_l:
-                if x[1] != 0:
-                    print("{}: {}".format(x[0], x[1]))
-
-    return None
+    if after is None:
+        if len(instances) == 0:
+            print("")
+            return
+        instances = sorted(instances.items(), key=lambda kv: (-kv[1], kv[0]))
+        [print("{}: {}".format(k, v)) for k, v in instances]
+    else:
+        count_words(subreddit, word_list, instances, after, count)
